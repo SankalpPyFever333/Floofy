63512e17494b1a53fe1c4ed162fd96b9bf0922d1
sankalp <sankalppandey696@gmail.com>
2024-03-27 15:14:41 +0530

    added the id og doctor in the match stage to get the data particular doctor

--- backend/Controllers/Doctor/ShowAppointRevDoctor/CalculateTotalRevenue.controller.js
+++ backend/Controllers/Doctor/ShowAppointRevDoctor/CalculateTotalRevenue.controller.js
@@ -1,89 +1,92 @@
+const { default: mongoose } = require("mongoose");
 const AppointmentModel = require("../../../Modals/DoctorModals/DoctorAppointment.modal");
 const getLastMonthStartDate = require("../../AdminController/getLastDates/getLastMonth");
 const getLastWeekDates = require("../../AdminController/getLastDates/getLastWeekDates");
 const getLastYear = require("../../AdminController/getLastDates/getLastYearDates");
+// const { ObjectId } = require("mongoose").Types;
 
 
-const CalTotalRevenue = async(getLastStartDate)=>{
-      try {
-            switch(getLastStartDate){
-                  case "Last Week":
-                        const { startDateWeek, endDateWeek } =  getLastWeekDates();
-                        const lastWeekRevenue = await AppointmentModel.aggregate([
-                              {
-                                    $match: {
-                                          createdAt:{
-                                                $gte: new Date(startDateWeek),
-                                                $lte : new Date(endDateWeek)
-                                          }
-                                    }
-                              },
-                              {
-                                    $group:{
-                                          _id: null,
-                                          totalPaymentAmount: {$sum:{$toInt:"$paymentAmount"} }
-                                    }
-                              }
-                        ]);
-                        return lastWeekRevenue;
+const CalTotalRevenue = async (getLastStartDate, doctorId) => {
+  try {
+    switch (getLastStartDate) {
+      case "Last Week":
+        const { startDateWeek, endDateWeek } = getLastWeekDates();
+        const lastWeekRevenue = await AppointmentModel.aggregate([
+          {
+            $match: {
+              createdAt: {
+                $gte: new Date(startDateWeek),
+                $lte: new Date(endDateWeek),
+              },
+              Doctor: new mongoose.Types.ObjectId(doctorId),
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              totalPaymentAmount: { $sum: { $toInt: "$paymentAmount" } },
+            },
+          },
+        ]);
+        return lastWeekRevenue;
 
-                  case "Last Month":
-                        const { startDateMonth, endDateMonth } = getLastMonthStartDate();
-                        const lastMonthRevenue =
-                          await AppointmentModel.aggregate([
-                            {
-                              $match: {
-                                createdAt: {
-                                  $gte: new Date(startDateMonth),
-                                  $lte: new Date(endDateMonth),
-                                },
-                              },
-                            },
-                            {
-                              $group: {
-                                _id: null,
-                                totalPaymentAmount: {
-                                  $sum: { $toInt: "$paymentAmount" },
-                                },
-                              },
-                            },
-                          ]);
-                          return lastMonthRevenue;
-                  case "Last Year":
-                        const { startDateYear, endDateYear } = getLastYear();
-                        const lastYEarRevenue =
-                          await AppointmentModel.aggregate([
-                            {
-                              $match: {
-                                createdAt: {
-                                  $gte: new Date(startDateYear),
-                                  $lte: new Date(endDateYear),
-                                },
-                              },
-                            },
-                            {
-                              $group: {
-                                _id: null,
-                                totalPaymentAmount: {
-                                  $sum: { $toInt: "$paymentAmount" },
-                                },
-                              },
-                            },
-                          ]);
-                          return lastYEarRevenue;
-                  default:
-            }
-      } catch (error) {
-            console.log(error);
-            
-      }
-}
+      case "Last Month":
+        const { startDateMonth, endDateMonth } = getLastMonthStartDate();
+        const lastMonthRevenue = await AppointmentModel.aggregate([
+          {
+            $match: {
+              createdAt: {
+                $gte: new Date(startDateMonth),
+                $lte: new Date(endDateMonth),
+              },
+              Doctor: new mongoose.Types.ObjectId(doctorId),
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              totalPaymentAmount: {
+                $sum: { $toInt: "$paymentAmount" },
+              },
+            },
+          },
+        ]);
+        return lastMonthRevenue;
+      case "Last Year":
+        const { startDateYear, endDateYear } = getLastYear();
+        const lastYEarRevenue = await AppointmentModel.aggregate([
+          {
+            $match: {
+              createdAt: {
+                $gte: new Date(startDateYear),
+                $lte: new Date(endDateYear),
+              },
+              Doctor: new mongoose.Types.ObjectId(doctorId),
+            },
+          },
+          {
+            $group: {
+              _id: null,
+              totalPaymentAmount: {
+                $sum: { $toInt: "$paymentAmount" },
+              },
+            },
+          },
+        ]);
+        return lastYEarRevenue;
+      default:
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const calTotalRevenueDoc = async(req, res)=>{
-      const { getLastStartDate } = req.body;
+      const { getLastStartDate , doctorId} = req.body;
+      console.log("docxtor id",doctorId)
       try {
-            const totalRev = await CalTotalRevenue(getLastStartDate);
-            console.log(totalRev);
+            const totalRev = await CalTotalRevenue(getLastStartDate, doctorId);
+            console.log("total rev",totalRev);
             if(totalRev){
                   res.status(200).json({message:"Total revenue fetched" , totalRev:totalRev})
             }
