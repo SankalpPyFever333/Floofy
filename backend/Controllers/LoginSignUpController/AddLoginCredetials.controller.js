d754c66b12ae60f5c8a80c5bb9920a47db557af8
sankalp <sankalppandey696@gmail.com>
2024-05-07 15:06:09 +0530

    updated authentication module

--- backend/Controllers/LoginSignUpController/AddLoginCredetials.controller.js
+++ backend/Controllers/LoginSignUpController/AddLoginCredetials.controller.js
@@ -6,6 +6,43 @@ const saltRounds = 10;
 const AddUserLoginCred = async (req, res)=>{
       let {username , password , contactNumber,  userType} = req.body;
       try {
+
+            const existingUsername = await userSchema.findOne({
+              username: username,
+            });
+            if (existingUsername) {
+              return res.status(400).json({ error: "Username already exists" , errorcode:"Username" });
+            }
+
+            // Hash the plain-text password
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+            // Check if the password already exists
+            const existingPassword = await userSchema.findOne({
+              password: hashedPassword,
+            });
+            if (existingPassword) {
+              return res
+                .status(400)
+                .json({
+                  error: "Password already exists",
+                  errorcode: "Password",
+                });
+            }
+
+            // Check if the contact number already exists
+            const existingContactNumber = await userSchema.findOne({
+              contactNumber: contactNumber,
+            });
+            if (existingContactNumber) {
+              return res
+                .status(400)
+                .json({
+                  error: "Contact number already exists",
+                  errorcode: "ContactNumber",
+                });
+            }
+
             bcrypt.hash(password , saltRounds, (err, hash)=>{
                   if(err){
                         console.error(err)
